6b1d3108d7e277f7ad41e96bfb7bcac65817f99b
abdelaziz20251 <abdelaziz20251@hotmail.com>
2025-04-19 14:01:11 +0200

    11

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,40 @@
-import Home from "./Pages/Home";
+// import Home from "./Pages/Home";
 
-const App = () =>{
+// const App = () =>{
 
-  return (
-    <>
-    <Home/>
-    </>
+//   return (
+//     <>
+//     <Home/>
+//     </>
     
-  )
+//   )
+// }
+// export default App;
+
+import { useState } from "react";
+
+const App = () => {
+  const [counter , setCounter] = useState(0);
+
+  const incrment = () =>{
+    setCounter(counter +1)
+  }
+  const deIncrment = () =>{
+    setCounter(counter -1)
+  }
+  const reset = () =>{
+    setCounter(0)
+  }
+
+return (
+<>
+  <div>
+    <h1>Counter : {counter}</h1> <br />
+    <button onClick = {incrment}>Incrment</button>
+    <button onClick = {deIncrment}>Decrment</button>
+    <button onClick = {reset}>Reset</button>
+  </div>
+</>
+)
 }
 export default App;
